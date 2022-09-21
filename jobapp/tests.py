2b8f6e5fc83248aa68a4d966611bd45e8257bde5
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-09-22 02:17:29 +0530

    add tests for list endpoint

--- jobapp/tests.py
+++ jobapp/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from jobapp.models import Application
+
+
+@pytest.mark.django_db
+@pytest.fixture(scope='function')
+def test_user(django_user_model):
+    return django_user_model.objects.create(email='test@user.com',
+                                            first_name='test',
+                                            last_name='user')
+
+
+@pytest.mark.django_db
+def testListApplication_noApplicationInDb_returnEmptyList(test_user):
+    # given
+    client = APIClient()
+    client.force_authenticate(test_user)
+
+    # when
+    response = client.get('/applications/', format='json')
+
+    # then
+    assert len(response.data) == 0
+
+
+@pytest.mark.django_db
+def testListApplication_oneApplicationInDb_oneApplicationInResponse(test_user):
+    # given
+    client = APIClient()
+    client.force_authenticate(test_user)
+
+    Application.objects.create(applicant=test_user,
+                               opening='1',
+                               selected=False,
+                               description='some desc')
+
+    # when
+    response = client.get('/applications/', format='json')
+
+    # then
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
