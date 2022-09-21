6898b0b1f8a04caf6b3488e408e196120670b9f1
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-09-21 20:49:46 +0530

    add applicantion model

--- jobapp/models.py
+++ jobapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Application(models.Model):
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
+    opening = models.CharField(max_length=25)     # should be a foreign key
+    selected = models.BooleanField(default=False)
+    description = models.TextField(null=True)
